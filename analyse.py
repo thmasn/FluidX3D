fcd9da748f97144e24113d5c9b2405ccef85a0cb
thmasn <thomaspahler@t-online.de>
2024-11-22 19:35:57 +0100

    add averaging

--- analyse.py
+++ analyse.py
@@ -5,28 +5,20 @@ from PIL import Image, ImageDraw
 
 # File path to the .dat file
 file_path = "2xforces.dat"
-scaleFacX = 8
 file_path = "1xforces.dat"
-scaleFacX = 4
 file_path = "0_5xforces.dat"
-scaleFacX = 2
 file_path = "0_25xforces.dat"
-scaleFacX = 1
 file_path = "0_25xforces_1.dat"
-scaleFacX = 1
 file_path = "1xforces3.dat"
-scaleFacX = 2
 file_path = "2xforces3.dat"
-scaleFacX = 4
 file_path = "./bin/export/halfTimeStep3/forces.dat"
 file_path = "./bin/export/origTimeStep3/forces.dat"
 file_path = "./bin/export/doubTimeStep3/forces.dat"
-scaleFacX = 1
 
 # Colors for each column
 colors = [(0,0,0), (200,0,0), (0,128,0), (0,0,255), (128,128,0), (0,128,128), (128,0,128)]
 
-# Read data from the file
+# Read data from the file, gives us an array of csv rows (time / x) with an array of values (x, y, z)
 data = []
 column_names = []
 with open(file_path, 'r') as file:
@@ -37,15 +29,39 @@ with open(file_path, 'r') as file:
         clean_row = [float(value) for value in row if value.strip()]
         data.append(clean_row)
 
-# Transpose data to access columns
+# Transpose data to access columns, gives us an array of values (x, y, z) with an array of data points (time / x)
 data = np.array(data).T
+
+# create average history
+startAverageX = int(len(data[0]) / 2)
+averaged_data = []
+totals = []
+sample = []
+
+for column in data: # go through each value
+    total = 0.0
+    samples = 0
+    averaged_points = []
+    for x in range(startAverageX, len(column)): # go through data points in second half
+        total += column[x]
+        samples += 1
+        averaged_points.append(total / samples)
+    totals.append(0.0)
+    sample.append(0)
+    averaged_data.append(averaged_points)
+
+# Prepare for plotting
+scaleFacY = 4 #10
+width = len(data[0])
+height = 200*scaleFacY
+background_color = (0, 0, 0)  # Black background
 
 # Mapping values from 0-40000 to 0-200
 def map_value(value):
     min_val = -5000
     max_val = 5000
-    new_min = 0
-    new_max = 200
+    new_min = height
+    new_max = 0
     return np.interp(value, [min_val, max_val], [new_min, new_max])
     #return new_min + (value - min_val) * (new_max - min_val) / (max_val - min_val)
     # map logarithmically with sign
@@ -61,12 +77,11 @@ mapped_data = []
 for column in data:
     mapped_column = [map_value(value) for value in column]
     mapped_data.append(mapped_column)
-
-# Prepare for plotting
-scaleFacY = 4 #10
-width = len(data[0])*scaleFacX
-height = 200*scaleFacY
-background_color = (0, 0, 0)  # Black background
+# Map averages data
+mapped_averaged_data = []
+for column in averaged_data:
+    mapped_column = [map_value(value) for value in column]
+    mapped_averaged_data.append(mapped_column)
 
 # Create an image
 image = Image.new("RGB", (width, height), background_color)
@@ -95,8 +110,8 @@ def draw_line(xy, fill, lineWidth = 1):
         else:
             pixels[interp_x, interp_y] = add_colors(pixels[interp_x, interp_y], fill)
 
-def drawHorizontalLine (y, color):
-    draw_line([(0, map_value(y)*scaleFacY), (width - 1, map_value(y)*scaleFacY)], fill=color)#, width=1
+def drawHorizontalLine(y, color):
+    draw_line([(0, map_value(y)), (width - 1, map_value(y))], fill=color)#, width=1
 
 # Add a horizontal white line in the middle of the image
 drawHorizontalLine(0, (128, 128, 128))
@@ -112,10 +127,18 @@ for col_idx, column_data in enumerate(mapped_data):
     for x in range(1, len(column_data)):
         #print(x)
         # Coordinates of the previous and current points
-        prev_x, prev_y = x, height - column_data[x - 1]*scaleFacY
-        curr_x, curr_y = x, height - column_data[x]*scaleFacY
+        prev_y = column_data[x - 1]
+        curr_y = column_data[x]
         # Draw a line connecting the points
-        draw_line([(prev_x, prev_y), (curr_x, curr_y)], fill=colors[col_idx % len(colors)])#, width=1
+        draw_line([(x, prev_y), (x, curr_y)], fill=colors[col_idx % len(colors)])#, width=1
+# Draw averages graph
+for col_idx, column_data in enumerate(mapped_averaged_data):
+    for x in range(startAverageX, len(column_data)):
+        # Coordinates of the previous and current points
+        prev_y = column_data[x - 1]
+        curr_y = column_data[x]
+        # Draw a line connecting the points
+        draw_line([(x, prev_y), (x, curr_y)], fill=colors[col_idx % len(colors)])#, width=1
 
 # Save the image
 output_file = file_path+"__.png"
